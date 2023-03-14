ab3b415cb0d242eed62e74fecbb1aabca30dbf68
naveenpatil1028 <121613773+naveenpatil1028@users.noreply.github.com>
2023-03-14 14:30:07 +0530

    UI test

---
+++ uiTest.js
@@ -0,0 +1,18 @@
+function longestConsec(strarr, k) {
+  if (strarr.length == 0 || k > strarr.length || k <= 0) return '';
+  
+  let longStr = '';
+  
+  let newStr = '';
+  
+  for (let i = 0; i < strarr.length; i++){
+    newStr = strarr.slice(i, i+k);
+    if (newStr.join('').length > longStr.length ){
+      longStr = newStr.join('');
+    }
+  }
+  console.log(longStr)
+ 
+}
+
+longestConsec( ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2) 
